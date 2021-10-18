77efc0c9604c7586f7e3beed6293f9c8242b7c7d
Garry Peffer <gpeffer98@gmail.com>
2021-10-18 13:40:30 -0400

    Added time functunality to to-do list.

--- task-tracking.sh
+++ task-tracking.sh
@@ -2,6 +2,8 @@
 
 FILE="/home/garry/.todo/todo-list.md"
 
+Current_time="$(date +%d-%b) @ $(date +%H:%M)"
+
 if [[ -f todo-list.md=true ]]
 	echo "Welcome to your daily dose of organization. Your list has been found."
 then
@@ -14,26 +16,26 @@ then
 		read answer
 
 			if [[ "${answer}" = "yes" ]]
-				then
-					echo "Ready to append..."
-					read append
-					echo "${append}" >> todo-list.md
-					echo "Displaying entries..."
-					sleep 1
-					nl todo-list.md
-					echo "Do you have another entry (yes/no) OR do you want to remove an entry (remove)?"
+			then
+				echo "Ready to append..."
+				read append
+				echo "${append} | created on "${Current_time}"" >> todo-list.md
+				echo "Displaying entries..."
+				sleep 1
+				nl todo-list.md
+				echo "Do you have another entry (yes/no) OR do you want to remove an entry (remove)?"
 			fi
 
 			if [[ "${answer}" = "remove" ]]
-				then
-					echo "Select line to remove (1,2,3,etc.)."
-					read remove
-					sed -i "${remove}"'d' "${FILE}"
-					echo "Line "${remove}" removed."
-					echo "Displaying entries..."
-					sleep 1
-					nl todo-list.md
-					echo "Do you have another entry (yes/no) OR do you want to remove an entry (remove)?"
+			then
+				echo "Select line to remove (1,2,3,etc.)."
+				read remove
+				sed -i "${remove}"'d' "${FILE}"
+				echo "Line "${remove}" removed on "${Current_time}"."
+				echo "Displaying entries..."
+				sleep 1
+				nl todo-list.md
+				echo "Do you have another entry (yes/no) OR do you want to remove an entry (remove)?"
 			fi
 
 		done
